c0a7b03bde94207af902a65bf03eb5cc6f0784b9
deconf <kilitary@gmail.com>
2020-08-22 05:08:41 +0300

    work

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,14 +8,14 @@ class PageController extends Controller
 {
     public function index(Request $request)
     {
-        \Log::debug($_SERVER['REMOTE_ADDR']);
+        \Log::debug($_SERVER['REMOTE_ADDR'] . ' ' . $request->url() . ' from ' . $request->header('HTTP_REFERER'));
         $info = $request->fullUrl();
         return view('home', compact('info'));
     }
 
     public function notfound(Request $request)
     {
-        \Log::debug($_SERVER['REMOTE_ADDR'] . ' ' . $request->url());
+        \Log::debug($_SERVER['REMOTE_ADDR'] . ' ' . $request->url() . ' from ' . $request->header('HTTP_REFERER'));
         $info = $request->fullUrl();
         return view('home', compact('info'));
     }
