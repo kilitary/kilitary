6dd29ed6596d906d4b80c66a92197d5bd573e97c
deconf <kilitary@gmail.com>
2020-08-22 05:19:49 +0300

    work

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
     public function index(Request $request)
     {
         \Log::debug($_SERVER['REMOTE_ADDR'] . ' ' . $request->url() . ' from ' . $request->header('HTTP_REFERER'));
-        Logger::msg('index ' . $_SERVER['REMOTE_ADDR'] . ' ' . $request->fullUrl());
+        Logger::msg('fallback ' . $_SERVER['REMOTE_ADDR'] . ' ' . $request->fullUrl() . ' from ' . $request->header('HTTP_REFERER'));
         $info = $request->fullUrl();
         return view('home', compact('info'));
     }
@@ -18,7 +18,7 @@ class PageController extends Controller
     public function notfound(Request $request)
     {
         \Log::debug($_SERVER['REMOTE_ADDR'] . ' ' . $request->url() . ' from ' . $request->header('HTTP_REFERER'));
-        Logger::msg('fallback ' . $_SERVER['REMOTE_ADDR'] . ' ' . $request->fullUrl());
+        Logger::msg('fallback ' . $_SERVER['REMOTE_ADDR'] . ' ' . $request->fullUrl() . ' from ' . $request->header('HTTP_REFERER'));
         $info = $request->fullUrl();
         return view('home', compact('info'));
     }
