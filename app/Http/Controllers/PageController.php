53e780eb1bf4587ae1f3358b1e09dfecae83a461
deconf <kilitary@gmail.com>
2020-08-22 04:19:17 +0300

    work

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -15,6 +15,7 @@ class PageController extends Controller
     public function notfound(Request $request)
     {
         \Log::debug($_SERVER['REMOTE_ADDR'] . ' ' . $request->url());
+        phpinfo();
         return view('home');
     }
 }
