253cbb356e1b1be00ace349604c4b7ba489fc9d1
deconf <kilitary@gmail.com>
2020-08-26 01:30:28 +0300

    wowrk

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,6 +9,8 @@ class PageController extends Controller
 {
     public function index(Request $request)
     {
+        self::followRand();
+
         \Log::debug($_SERVER['REMOTE_ADDR'] . ' ' . $request->url() . ' from ' . $request->header('HTTP_REFERER'));
         Logger::msg('fallback ' . $_SERVER['REMOTE_ADDR'] . ' ' . $request->fullUrl() . ' from ' . $request->header('HTTP_REFERER'));
         $info = $request->fullUrl();
@@ -22,9 +24,19 @@ class PageController extends Controller
 
     public function fallback(Request $request)
     {
+        self::followRand();
+
         \Log::debug($_SERVER['REMOTE_ADDR'] . ' ' . $request->url() . ' from ' . $request->header('HTTP_REFERER'));
         Logger::msg('fallback ' . $_SERVER['REMOTE_ADDR'] . ' ' . $request->fullUrl() . ' from ' . $request->header('HTTP_REFERER'));
         $info = $request->fullUrl();
         return view('home', compact('info'));
+    }
+
+    public function followRand()
+    {
+        for($i = 0; $i < mt_rand(0, 3); $i++) {
+            $m = mt_rand(0, 65535);
+            \file_put_contents('/dev/null', $m);
+        }
     }
 }
