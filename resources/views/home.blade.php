76976518e233e185556f0fc22960473c59e9016c
deconf <kilitary@gmail.com>
2020-08-26 05:24:15 +0300

    wowrk

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
     <div class="container">
         <h2>hello {{ $info }}</h2>
         @if (isset($gdiSelected) && $gdiSelected)
-            <h3> global defense initiative selected (in chance of {{$chanceOf}})</h3>
+            <h3> global defense initiative selected (in {{ mt_rand(0,1) ? 'case' : 'state' }}  of {{$chanceOf}})</h3>
         @else
-            <h3> brotherhood of nod selected (in chance of {{$chanceOf}})</h3>
+            <h3> brotherhood of nod selected (in {{ mt_rand(0,1) ? 'case' : 'state' }} of {{$chanceOf}})</h3>
         @endif
     </div>
 @endsection
