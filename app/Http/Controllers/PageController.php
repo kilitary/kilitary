f8aa2fee4a219d3144175e0f22834f090bbb5ac0
deconf <kilitary@gmail.com>
2020-08-26 05:30:16 +0300

    wowrk

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,39 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use \App\Logger;
+use \App\XRandom;
 
 class PageController extends Controller
 {
     public function index(Request $request)
     {
-        self::followRand();
+        XRandom::followRand();
 
         \Log::debug($_SERVER['REMOTE_ADDR'] . ' ' . $request->url() . ' from ' . $request->header('HTTP_REFERER'));
         Logger::msg('fallback ' . $_SERVER['REMOTE_ADDR'] . ' ' . $request->fullUrl() . ' from ' . $request->header('HTTP_REFERER'));
         $info = $request->fullUrl();
 
-        $gdiSelected = mt_rand(0, 1);
-        $chanceOf = mt_rand(0, 4);
-        Logger::msg($gdiSelected);
-        // one run
+        $gdiSelected = XRandom::get(0, 1);
+        $chanceOf = XRandom::get(0, 4);
+
+        Logger::msg('$gdiSelected:  ' . $gdiSelected . ' $chanceOf: ' . $chanceOf);
+
         return view('home', compact('info', 'gdiSelected', 'chanceOf'));
     }
 
     public function fallback(Request $request)
     {
-        self::followRand();
+        XRandom::followRand();
 
         \Log::debug($_SERVER['REMOTE_ADDR'] . ' ' . $request->url() . ' from ' . $request->header('HTTP_REFERER'));
         Logger::msg('fallback ' . $_SERVER['REMOTE_ADDR'] . ' ' . $request->fullUrl() . ' from ' . $request->header('HTTP_REFERER'));
         $info = $request->fullUrl();
+
         return view('home', compact('info'));
     }
 
-    public function followRand()
-    {
-        for($i = 0; $i < mt_rand(0, 3); $i++) {
-            $m = mt_rand(0, 65535);
-            \file_put_contents('/dev/null', $m);
-        }
-    }
 }
