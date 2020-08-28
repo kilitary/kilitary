bc3a034fcad36ace69da98e469caf35a8ac90f07
deconf <kilitary@gmail.com>
2020-08-29 01:19:18 +0300

    wowrk

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -4,8 +4,8 @@
     <div class="container">
         @foreach ($list as $key => $item)
             <div class="list-item-container">
-                <span class="list-key">{{ $key }}</span>: <span
-                    class="list-item">{!! $item !!}</span>
+                <span class="list-key">{{ $key }}</span>
+                <span class="list-item">{!! $item !!}</span>
             </div>
         @endforeach
     </div>
