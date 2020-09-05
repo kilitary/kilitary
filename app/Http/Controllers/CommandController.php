25edeb1d83bd952e9663dba3387a83082438fc47
deconf <kilitary@gmail.com>
2020-09-05 03:34:59 +0300

    www

--- app/Http/Controllers/CommandController.php
+++ app/Http/Controllers/CommandController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use App\XRandom;
 use Illuminate\Http\Request;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 class CommandController extends Controller
 {
-    public function sync()
+    public function sync(Request $request)
     {
         $list['rand'] = \strtoupper(XRandom::sign(26));//::getAu(26);
 
@@ -16,7 +19,7 @@ class CommandController extends Controller
             $list[$algo] = $hash . " <a target=_blank href='https://google.com/search?q=%2B" . substr($hash, 0, 7) . "'>[?]</a>" .
                 " <a target=_blank href='https://google.com/search?q=related:" . substr($hash, 0, 6) . "'>[I]</a>" .
                 " <a target=_blank href='https://google.com/search?q=inurl:" . substr($hash, 0, 7) . "'>[L]</a>" .
-                " <a target=_blank href='https://google.com/search?q=" . substr($hash, 0, 6) . "'>[E]</a>".
+                " <a target=_blank href='https://google.com/search?q=" . substr($hash, 0, 6) . "'>[E]</a>" .
                 " <a target=_blank href='https://google.com/search?q=site:github.com%20" . substr($hash, 0, 6) . "'>[C]</a>";
 
         }
@@ -31,7 +34,7 @@ class CommandController extends Controller
         return view('list', compact('list'));
     }
 
-    public function play()
+    public function play(Request $request)
     {
         $val = XRandom::getAu(26);
         $list['val'] = $val;
@@ -41,9 +44,48 @@ class CommandController extends Controller
 
     public function command(Request $request, $command)
     {
-        $ret = self::$command();
+        $ret = self::$command($request);
 
         return $ret;
+    }
+
+    public function mail(Request $request)
+    {
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            //$mail->isSMTP();                                            // Send using SMTP
+            //$mail->Host = 'smtp1.example.com';                    // Set the SMTP server to send through
+            //$mail->SMTPAuth = true;                                   // Enable SMTP authentication
+            //$mail->Username = 'user@example.com';                     // SMTP username
+            //$mail->Password = 'secret';                               // SMTP password
+            //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            //$mail->Port = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+            //Recipients
+            $mail->setFrom('root@kilitary.ru', 'Mailer');
+            $mail->addAddress('deconf@ya.ru', 'Joe User');     // Add a recipient
+            $mail->addReplyTo('info@example.com', 'Information');
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
+
+            // Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $ret = $mail->send();
+            return ['status' => $ret];
+        } catch(Exception $e) {
+            return ['status' => $mail->ErrorInfo];
+        }
     }
 }
 
