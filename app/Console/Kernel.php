e410cb77e50d9f0f5cbcf00f0774ee90a526520c
deconf <kilitary@gmail.com>
2020-09-07 00:33:11 +0300

    в  ваще

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,12 +19,17 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     *
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function() {
+            DB::table('short_urls')
+                ->where("long", "NOT like", '%http%')
+                ->delete();
+        })->daily();
     }
 
     /**
@@ -34,7 +39,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
