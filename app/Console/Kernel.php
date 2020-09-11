df95e4a421172a1b9f73a48b79a175c7d9eeea38
deconf <kilitary@gmail.com>
2020-09-11 20:03:06 +0300

    пиздец

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends ConsoleKernel
             DB::table('short_urls')
                 ->where("long", "not like", '%http%')
                 ->delete();
-        })->everyMinute();
+        })->everyFourHours();
     }
 
     /**
