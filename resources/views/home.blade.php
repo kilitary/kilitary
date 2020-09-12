2ca1a82cfa6f703b632ed561976a4b2214fa914b
deconf <kilitary@gmail.com>
2020-09-12 04:11:58 +0300

    пиздец

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,26 @@
 
 @section('content')
     <div class="container">
-        <h2>hello {{ $info }}</h2>
-        @if (isset($gdiSelected) && $gdiSelected)
-            <h3> global defense initiative selected (in {{ \App\XRandom::scaled(0,1) ? 'case' : 'state' }} of {{$chanceOf}}
-                )</h3>
-        @else
-            <h3> brotherhood of nod selected (in {{ \App\XRandom::scaled(0,1) ? 'case' : 'state' }} of {{$chanceOf}})</h3>
-        @endif
 
-        @if (empty($sign))
-            <span color=red class="blinking-red"><< empty >></span>
-        @else
-            <div style="font-size:13px;font-variant: small-caps;font-family: consolas">sign {!! $sign !!}</div>
-        @endif
-        <div class="links"><a href='/command/sync?{{request()->path()}}'>sync?</a></div>
+        <h2>hello {{ $info }}</h2>
+        <div class="marginleft">
+            @if (isset($gdiSelected) && $gdiSelected)
+                <h3> global defense initiative selected (in {{ \App\XRandom::scaled(0,1) ? 'case' : 'state' }}
+                    of {{$chanceOf}}
+                    )</h3>
+            @else
+                <h3> brotherhood of nod selected (in {{ \App\XRandom::scaled(0,1) ? 'case' : 'state' }} of {{$chanceOf}}
+                    )</h3>
+            @endif
+
+
+            @if (empty($sign))
+                <span color=red class="blinking-red"><< empty >></span>
+            @else
+                <div style="font-size:13px;font-variant: small-caps;font-family: consolas">sign {!! $sign !!}</div>
+            @endif
+            <div class="links"><a href='/command/sync?{{request()->path()}}'>sync?</a></div>
+        </div>
 
         <div class="shortener">
             <form method="post" action="/us/create">
@@ -26,6 +32,10 @@
                     <button>add short/long url translation (url shortener) or just click this button</button>
                 </div>
             </form>
+        </div>
+
+        <div class="marginleft">
+            {!! nl2br(`/usr/bin/timedatectl`) !!}
         </div>
 
         <div>
