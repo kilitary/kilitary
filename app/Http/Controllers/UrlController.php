add3a70caeafd31378961ad5404b5b3ce84748bf
deconf <kilitary@gmail.com>
2020-09-12 11:52:47 +0300

    пиздец

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -49,7 +49,7 @@ class UrlController extends Controller
             $shortRecord->save();
             return redirect($shortRecord->long);
         } else {
-            Logger::msg($shortUrl . ' => ' . $_SERVER['HTTP_REFERER'] . ' by ' . $request->ip());
+            Logger::msg($shortUrl . ' => ' . $request->header('HTTP_REFERER', 'http://google.com') . ' by ' . $request->ip());
             return back();
         }
     }
