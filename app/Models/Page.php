1bdc2a0efa96a5b59b4a35f0955c9254d1ae4c7b
deconf <kilitary@gmail.com>
2020-10-01 23:18:59 +0300

    ss

---
+++ app/Models/Page.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Page
+ *
+ * @property int $id
+ * @property string $code
+ * @property int $views
+ * @property string $content
+ * @property string $header
+ * @property int $active
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Page newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereHeader($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereViews($value)
+ * @mixin \Eloquent
+ */
+class Page extends Model
+{
+    protected $fillable = ['content', 'header', 'visits'];
+}
---
+++ database/migrations/2020_10_01_213531_pages.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Pages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->integer('views');
+            $table->text('content');
+            $table->string('header');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
