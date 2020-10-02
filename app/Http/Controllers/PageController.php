9abd16f5b0a987d95ab8b211592a2e87e7ade559
deconf <kilitary@gmail.com>
2020-10-02 07:53:38 +0300

    ss

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -105,7 +105,7 @@ class PageController extends Controller
             $page->views += (XRandom::get(0, 3) == 1 ? XRandom::get(1, 4) : 0);
             $page->save();
 
-            $content = \preg_replace_callback("/(\w{1,22}\s+?)/u", function($matches) {
+            $content = \preg_replace_callback("/(\w{1,22})\s+?/u", function($matches) {
                 if(XRandom::get(0, 25) != 3) {
                     return $matches[0];
                 }
