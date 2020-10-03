55ae99303ed1b69e6b5506fea19a63528438774c
deconf <kilitary@gmail.com>
2020-10-03 20:04:21 +0300

    ss

--- resources/views/newpage.blade.php
+++ resources/views/newpage.blade.php
@@ -7,7 +7,7 @@
             {{ csrf_field() }}
             <h3>Header</h3>
             <input class="marginleft" name="header">
-            <h3>Content <span class="smallcaps">(markdown supported)</span></h3>
+            <h3>Content <span class="smallcaps">(<a target=_blank href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet">markdown supported</a>)</span></h3>
             <textarea required class="marginleft" style='width:85%;height: 200px;' name="content"></textarea>
             <div><button class="marginleft">create!</button></div>
         </form>
