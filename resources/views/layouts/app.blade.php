ac2bd7f271058eb618a948ddc70bb2985630899c
deconf <kilitary@gmail.com>
2020-10-11 14:19:22 +0300

    ss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,40 @@
           content="{{$description ?? 'v2k, military, fss, fbi, cia, nsa, nasa, intrepol, thales of the futur e'}}">
     <meta name="generator" content="laraGEN">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="yandex-verification" content="0c0bf1373a8046f1"/>
 
     <title>{{ env('APP_NAME') }}</title>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {(m[i].a = m[i].a || []).push(arguments);};
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(68175511, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true,
+            trackHash: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/68175511" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-179562632-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {dataLayer.push(arguments);}
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-179562632-1');
+    </script>
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
@@ -34,17 +66,6 @@
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
         crossorigin="anonymous"></script>
 <script src="{{ asset('js/app.js') }}"></script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-179562632-1"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {dataLayer.push(arguments);}
-
-    gtag('js', new Date());
-
-    gtag('config', 'UA-179562632-1');
-</script>
 
 @yield('scripts')
 </body>
