edbbeebf8cf68042b5d0149c2d10e58e4fd33cf0
deconf <kilitary@gmail.com>
2020-10-17 14:55:33 +0300

    ss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,7 +70,7 @@
 <footer>
     <div style="float:right;position: absolute;top: 10px;left: 100px">
         <a target=_blank href="/images/operatoR.jpg">
-            <img class="crysa-class" title="Crysa class server admin (Moulder IV 7++)" src="/images/krisa.png"></a>
+            <img class="crysa-class" title="Crysa class server admin (Moulder IV 7+)" src="/images/krisa.png"></a>
     </div>
 </footer>
 @yield('scripts')
