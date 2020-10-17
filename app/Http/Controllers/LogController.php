6b5e39e409a533bc81e462b5ff70d5b1f89a17b6
deconf <kilitary@gmail.com>
2020-10-17 16:19:01 +0300

    ss

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -10,6 +10,7 @@ class LogController extends Controller
     public function index(Request $request)
     {
         $logs = \App\Models\LogRecord::select('id', 'ip', 'http_code', 'method', 'url', 'ua', 'info', 'created_at')
+            ->limit(10)
             ->latest()
             ->get();
 
