0712cafc902558f7fdd20fc90a7702e988e1efb7
deconf <kilitary@gmail.com>
2020-10-18 19:07:16 +0300

    ss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="yandex-verification" content="0c0bf1373a8046f1"/>
 
-    <link rel="icon" type="image/png" href="https://kilitary.ru/images/logo.png">
+    <link rel="icon" type="image/png" href="https://kilitary.ru/images/favicon.png">
     <link rel="icon" href="https://kilitary.ru/favicon.ico" type="image/x-icon">
 
     <title>{{ env('APP_NAME') }}</title>
