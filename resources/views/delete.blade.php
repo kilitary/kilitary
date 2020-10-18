abcb3c416079d191a57f048aaec1cf5ef65afaeb
deconf <kilitary@gmail.com>
2020-10-18 10:26:20 +0300

    ss

--- resources/views/delete.blade.php
+++ resources/views/delete.blade.php
@@ -17,11 +17,11 @@
 @section('scripts')
     <script>
         $(function() {
-            for(let i = 0; i < rando(2, 5); i++) {
+            for(let i = 0; i < rando(2, 8); i++) {
                 setTimeout(function() {
                     var audio = new Audio('/audio/HALTSND.mp3');
                     audio.play();
-                }, rando(400, 900));
+                }, rando(400, 1300));
 
             }
         });
