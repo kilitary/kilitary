2e44c20dac317b5c7651a69a159a8aa1d085c0bc
deconf <kilitary@gmail.com>
2020-10-18 04:18:03 +0300

    ss

--- config/session.php
+++ config/session.php
@@ -33,7 +33,7 @@ return [
 
     'lifetime' => env('SESSION_LIFETIME', 120),
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -196,6 +196,6 @@ return [
     |
     */
 
-    'same_site' => null,
+    'same_site' => "lax",
 
 ];
