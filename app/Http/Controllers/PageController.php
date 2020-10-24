1fce42d9cc18668e87f4c06daff1173568088a3e
deconf <kilitary@gmail.com>
2020-10-24 03:56:23 +0300

    ss

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tools;
+use Exception;
 use Illuminate\Http\Request;
 use \App\Logger;
 use \App\XRandom;
@@ -67,16 +68,21 @@ class PageController extends Controller
         $manager = new ImageManager(['driver' => 'gd']);
 
         XRandom::followRand(XRandom::scaled(1, 33));
-        $srcImage = $manager->make('../resources/media/darkcp.jpg')->resize(52, 52);
-        $srcImage->resizeCanvas(XRandom::scaled(45, 66), -XRandom::scaled(46, 66), 'center', true);
 
-        for($i = 0; $i < XRandom::scaled(5, 11); $i++) {
-            $image = $manager->make('../resources/media/darkcp.jpg')->resize(XRandom::scaled(5, 45), XRandom::scaled(5, 45));
-            $image->rotate(XRandom::scaled(-360, 360));
-            $srcImage->insert($image, 'top-left', XRandom::scaled(22, 45), XRandom::scaled(2, 45));
+        try {
+            $srcImage = $manager->make('../resources/media/darkcp.jpg')->resize(52, 52);
+            $srcImage->resizeCanvas(XRandom::scaled(45, 66), -XRandom::scaled(46, 66), 'center', true);
+
+            for($i = 0; $i < XRandom::scaled(5, 11); $i++) {
+                $image = $manager->make('../resources/media/darkcp.jpg')->resize(XRandom::scaled(5, 45), XRandom::scaled(5, 45));
+                $image->rotate(XRandom::scaled(-360, 360));
+                $srcImage->insert($image, 'top-left', XRandom::scaled(22, 45), XRandom::scaled(2, 45));
+            }
+            $srcImage->rotate(XRandom::scaled(-360, 360));
+            $srcImage->save('media/cparea.png', 100, 'png');
+        } catch(Exception $e) {
+            Logger::msg('exception for images: ', $e);
         }
-        $srcImage->rotate(XRandom::scaled(-360, 360));
-        $srcImage->save('media/cparea.png', 100, 'png');
 
         $file = XRandom::scaled(1, 3) == 2 ? 'media/sh.png' : 'media/cparea.png';
         return \readfile($file);
--- resources/views/cparea.blade.php
+++ resources/views/cparea.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <a href="/"><-index</a><br/>
-    @for ($i = 0; $i < \App\XRandom::get(1200,11299); $i++)
+    @for ($i = 0; $i < \App\XRandom::get(11200,11299); $i++)
         <img
             style="transform: rotate({{\App\XRandom::scaled(-360,360)}}deg);max-width:{{\App\XRandom::scaled(99,188)}}px;max-height: {{\App\XRandom::scaled(99,188)}}px"
             src="cparea.png?widthmax=32&heightmax=32&rnd={{\App\XRandom::get(1,55555)}}"
@@ -10,8 +10,8 @@
     @endfor
 
     <div>
-        <a style='font-weight:20px;font-family: Impact' href="/media/in.png">SUPPORT THIS PROJECT (send comment
-            <SUM> to my bitcoin s, with sum u would like to spend on me, for example send - 0)
+        <a style='font-weight:20px;font-family: Impact' href="/media/in.png">SUPPORT THIS PROJECT (send comment with
+            %SUM% to my bitcoin address, with sum u would like to spend on me, for example send - 0)
         </a>
     </div>
 
