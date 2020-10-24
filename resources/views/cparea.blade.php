8b25158dbe1e27723a4b8431a806935f5870e21d
deconf <kilitary@gmail.com>
2020-10-24 07:27:32 +0300

    ss

--- resources/views/cparea.blade.php
+++ resources/views/cparea.blade.php
@@ -5,7 +5,7 @@
     @for ($i = 0; $i < \App\XRandom::get(1120,1129); $i++)
         <img
             style="transform: rotate({{\App\XRandom::scaled(-360,360)}}deg);max-width:{{\App\XRandom::scaled(9,288)}}px;max-height: {{\App\XRandom::scaled(9,288)}}px"
-            src="cparea.png?widthmax=32&heightmax=32&rnd={{\App\XRandom::get(1,55555)}}"
+            src="cparea.rng?widthmax=32&heightmax=32&rnd={{\App\XRandom::get(1,55555)}}"
             title="dark microsoft members cp area #{{\App\XRandom::scaled(1360,1360)}}">
     @endfor
 
