22b083ea2da96675f63a5aa7b97cf552c9bac5c0
deconf <kilitary@gmail.com>
2020-10-24 09:08:25 +0300

    ss

--- app/Http/Middleware/PostRequest.php
+++ app/Http/Middleware/PostRequest.php
@@ -32,7 +32,8 @@ class PostRequest
                 ]);
         }
 
-        return $response->header('Cache-Control', 'no-cache, no-store, must-revalidate')
+        return $response
+            ->header('Cache-Control', 'no-cache, no-store, must-revalidate')
             ->header('Pragma', 'no-cache')
             ->header('X-CurlyBlank', 'Dont Be A Dick');;
     }
