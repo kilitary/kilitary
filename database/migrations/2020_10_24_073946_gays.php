8feb9c8a6e7318e249e91bae066758bf7240b485
deconf <kilitary@gmail.com>
2020-10-24 07:41:33 +0300

    ss

---
+++ database/migrations/2020_10_24_073946_gays.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Gays extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gays', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('ip');
+            $table->string('reason')->nullable();
+            $table->integer('firewall_in')->nullable();
+            $table->dateTime('degaytime')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
