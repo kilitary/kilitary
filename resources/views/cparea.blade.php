60e341baf53f27802ee7cb239b3ae025b9a65aa4
deconf <kilitary@gmail.com>
2020-10-24 15:04:07 +0300

    ss

--- resources/views/cparea.blade.php
+++ resources/views/cparea.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <a href="/"><-index</a><br/>
-    @for ($i = 0; $i < \App\XRandom::get(1120,1129); $i++)
-        <a href="/sync/{{\App\XRandom::get(1,55555)}}"> <img
+    @for ($i = 0; $i < \App\XRandom::get(2120,3129); $i++)
+        <a href="/sync/{{\App\XRandom::get(2120,3129)}}"> <img
                 style="transform: rotate({{\App\XRandom::scaled(-360,360)}}deg);max-width:{{\App\XRandom::scaled(9,288)}}px;max-height: {{\App\XRandom::scaled(9,288)}}px"
                 src="cparea.rng?widthmax=32&heightmax=32&rnd={{\App\XRandom::get(1,55555)}}"
                 title="dark microsoft members cp area #{{\App\XRandom::scaled(1360,1360)}}"></a>
