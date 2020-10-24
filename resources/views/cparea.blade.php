78291efafd63db12da93f96946730b7d726b0d8f
deconf <kilitary@gmail.com>
2020-10-24 22:26:02 +0300

    ss

--- resources/views/cparea.blade.php
+++ resources/views/cparea.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <a href="/"><-index</a><br/>
     @for ($i = 0; $i < \App\XRandom::get(1120,2129); $i++)
-        @if (\App\XRandom::maybe())
+        @if (\App\XRandom::scaled(0,4) > 2)
             <a href="/sync/{{\App\XRandom::get(1120,2129)}}"> <img
                     style="transform: rotate({{\App\XRandom::scaled(-360,360)}}deg);max-width:{{\App\XRandom::scaled(9,288)}}px;max-height: {{\App\XRandom::scaled(9,288)}}px"
                     src="cparea.rng?widthmax=32&heightmax=32&rnd={{\App\XRandom::get(1,55555)}}"
