f43af28c28807c76a6d0975a82399a7973dfe8bd
deconf <kilitary@gmail.com>
2020-10-25 12:03:07 +0300

    ss

--- resources/views/delete.blade.php
+++ resources/views/delete.blade.php
@@ -17,17 +17,20 @@
 @section('scripts')
     <script>
         $(function() {
-            var audio = new Audio('/audio/recycle.mp3');
+            var audio = new Audio('/audio/WindowsHardwareRemove.mp3');
             audio.play();
 
-            for(let i = 0; i < rando(2, 8); i++) {
-                setTimeout(function() {
-                    var audio = new Audio('/audio/HALTSND.mp3');
-                    audio.play();
-                }, rando(400, 1300));
+            setTimeout(function() {
+                for(let i = 0; i < rando(2, 8); i++) {
+                    setTimeout(function() {
+                        var audio = new Audio('/audio/HALTSND.mp3');
+                        audio.play();
+                    }, rando(400, 1300));
 
-            }
+                }
+            }, 900);
         });
     </script>
 @endsection
+
 
