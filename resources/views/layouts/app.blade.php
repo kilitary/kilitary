cf881f871cfbd18044077a8148214fb728643ee9
deconf <kilitary@gmail.com>
2020-10-30 23:37:42 +0300

    beautiful

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" xml:lang="ten" manifest="/cache.manifest">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" xml:lang="王宇ー！のドッ" manifest="/cache.manifest">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
