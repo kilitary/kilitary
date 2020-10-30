4124a232518fe25348281a089ce0992d29a735d7
deconf <kilitary@gmail.com>
2020-10-30 19:59:35 +0300

    beautiful

--- resources/views/cparea.blade.php
+++ resources/views/cparea.blade.php
@@ -7,7 +7,7 @@
             <a href="/sync/{{\App\XRandom::get(1120,3129)}}"> <img
                     style="transform: rotate({{\App\XRandom::scaled(-360,360)}}deg);max-width:{{\App\XRandom::scaled(9,288)}}px;max-height: {{\App\XRandom::scaled(9,288)}}px"
                     src="cparea.rng?widthmax={{\App\XRandom::scaled(99,130)}}&heightmax={{\App\XRandom::scaled(99,130)}}&rnd={{\App\XRandom::scaled(1,55555)}}"
-                    title="{{ \App\XRandom::maybe() ? 'dark members cp area #'. \App\XRandom::scaled(-1360,1360) : 'beautiful'}}"></a>
+                    title="{{ \App\XRandom::maybe() ? 'dark members cp area #'. \App\XRandom::scaled(-1360,1360) : 'beautiful ' . \App\XRandom::scaled(-1360,1360)}}"></a>
         @endif
     @endfor
 
@@ -17,7 +17,7 @@
             auction, send ru S services price for terrorizing me/per hour)
         </a>
         <div>
-            gays in the database: {{ $gays->count() }}
+            gays in database: {{ $gays->count() }}
         </div>
     </div>
 
