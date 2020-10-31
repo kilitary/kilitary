c4b8cfcd12238ec2b2747560747b852efb1ddfe3
deconf <kilitary@gmail.com>
2020-10-31 08:04:26 +0300

    beautiful

--- app/Audio.php
+++ app/Audio.php
@@ -16,6 +16,6 @@ class Audio extends Model
 {
     public static function gayDetected()
     {
-        `/usr/bin/ffplay -autoexit -vn -nodisp /home/kilitary/kilitary/storage/mp3/b.mp3`;
+        `/usr/bin/ffplay -autoexit -vn -nodisp /home/kilitary/kilitary/storage/mp3/b.mp3 &`;
     }
 }
