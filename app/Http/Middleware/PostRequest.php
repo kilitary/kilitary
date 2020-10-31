853e9874718197bab8f25dd6cda41a5a336fb564
deconf <kilitary@gmail.com>
2020-10-31 21:47:12 +0300

    beautiful

--- app/Http/Middleware/PostRequest.php
+++ app/Http/Middleware/PostRequest.php
@@ -63,6 +63,7 @@ class PostRequest
             ->header("Via", "%[^ ]*%20\"" . \str_repeat('\\', XRandom::scaled(1, 99)) . "" . XRandom::scaled(1, 999) . ", s + `--" . Xrandom::scaled(19999999, 999999999))
             ->header("X-Powered-By", "PHP/4.0.6")
             ->header('X-Gay', (int) $isGay)
+            ->header('X-At-War', 0)
             ->header("Server", "thttpd/0.3b");
     }
 }
