fd02c47b7944d2a0cab50a4a6859c58c0254bbf2
deconf <kilitary@gmail.com>
2020-11-07 14:51:57 +0300

    beautifull

--- app/Proxy.php
+++ app/Proxy.php
@@ -38,6 +38,6 @@ class Proxy extends Model
 {
     protected $fillable = ['host', 'ip', 'type', 'checked_at', 'anonymity'];
     public $table = 'proxys';
-    protected $dates = ['checked_at', 'added_at'];
+    protected $dates = ['checked_at', 'added_at', 'updated_at'];
     public $timestamps = true;
 }
