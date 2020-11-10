6be7c4a33cb933c04f6bf7fa2b53ef88ef36e18a
deconf <kilitary@gmail.com>
2020-11-10 22:21:02 +0300

    beautifull

--- resources/views/gays.blade.php
+++ resources/views/gays.blade.php
@@ -3,12 +3,22 @@
 @section('content')
     <a href="/"><-index</a><br/>
     <div class="container page-content">
-        @foreach ($gays as $gay)
-            <div>
-                {{$gay->ip}}: {{$gay->reason}}
-            </div>
-        @endforeach
-
+        <table>
+            <tr>
+                <th>ip</th>
+                <th>пушо</th>
+            </tr>
+            @foreach ($gays as $gay)
+                <tr>
+                    <td>
+                        {{$gay->ip}}
+                    </td>
+                    <td>
+                        {{$gay->reason}}
+                    </td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 
 @endsection
