1504d769240c1754698feb640488d239caf5f065
deconf <kilitary@gmail.com>
2020-11-10 22:24:14 +0300

    beautifull

--- resources/views/gays.blade.php
+++ resources/views/gays.blade.php
@@ -19,6 +19,10 @@
                 </tr>
             @endforeach
         </table>
+        <div>
+            Debun info - ask admin via <a href="mailto:deconf@kilitary.ru">email</a>, or create <a
+                href="/view/debun-info-post-thre">comment</a>.
+        </div>
     </div>
 
 @endsection
