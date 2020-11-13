e19d6f6f10b83861733ca5fe930e6526d97bdc6d
deconf <kilitary@gmail.com>
2020-11-14 01:11:08 +0300

    beautifull

--- app/Logger.php
+++ app/Logger.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -16,16 +17,20 @@ class Logger extends Model
 {
     public static function msg(...$msgs)
     {
-        foreach($msgs as $msg) {
-            if(\is_array($msg) || \is_object($msg)) {
-                $msg = \json_encode($msg, JSON_PRETTY_PRINT);
+        try {
+            foreach($msgs as $msg) {
+                if(\is_array($msg) || \is_object($msg)) {
+                    $msg = \json_encode($msg, JSON_PRETTY_PRINT);
+                }
+                $fp = fopen(env('LOG_FILE'), 'a');
+                if($fp) {
+                    $at = \Carbon::now()->format('Y-m-d H:i:s.u');
+                    fwrite($fp, '[' . sprintf("%06d", getmypid()) . '] ' . $at . ' ' . env('APP_NAME') . ': ' . $msg . PHP_EOL);
+                    fclose($fp);
+                }
             }
-            $fp = fopen(env('LOG_FILE'), 'a');
-            if($fp) {
-                $at = \Carbon::now()->format('Y-m-d H:i:s.u');
-                fwrite($fp, '[' . sprintf("%06d", getmypid()) . '] ' . $at . ' ' . env('APP_NAME') . ': ' . $msg . PHP_EOL);
-                fclose($fp);
-            }
+        } catch(Exception $e) {
+            Log::critical('fail to use Logger::msg(): ' . $e->getMessage());
         }
     }
 }
