9db2cf892aa8289d3bea03a4bf7da982647e6f17
deconf <kilitary@gmail.com>
2020-11-14 12:56:50 +0300

    beautifull

--- config/site.php
+++ config/site.php
@@ -4,6 +4,6 @@ return [
     'first_phase_session_lifetime' => 20,
     'first_phase_cstring_length' => 20,
     'session_lifetime_hours' => 12,
-    'internal_debug' => true,
+    'internal_debug' => false,
     'version' => 'v0.1'
 ];
